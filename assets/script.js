a62bbe035c235c27c4c3a24d6c5e55b434132d3a
EmanuelleSalesG <emanuellesalesg@gmail.com>
2023-10-31 15:29:19 -0300

    page structure using js part2

--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,13 @@ const docAll = (element) => document.querySelectorAll(element)
 pizzaJson.map((item, index)=>{
     
     let pizzaItem = doc('.models .pizza-item').cloneNode(true);
+
+    pizzaItem.querySelector('.pizza-item--img img').setAttribute('src', item.img);
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = "R$ " + item.price.toFixed(2);
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     
     doc('.pizza-area').appendChild(pizzaItem);
-
 
 });
 
